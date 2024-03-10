4a78fb358f4024d08205f29851b7f30d1d7b62d6
bean217 <benpiro1118@gmail.com>
2024-03-10 17:49:17 +0100

    Started created bindings for FreifelderViewModel.cs.

--- LeichtNote/Models/SettingsModels/FreifelderModel.cs
+++ LeichtNote/Models/SettingsModels/FreifelderModel.cs
@@ -2,5 +2,25 @@ namespace LeichtNote.Models.SettingsModels;
 
 public class FreifelderModel
 {
+    private string? _name { get; set; }
+
+    public string? Name
+    {
+        get { return _name; }
+        set
+        {
+            _name = value;
+        }
+    }
     
+    private bool? _enabled { get; set; }
+
+    public bool? Enabled
+    {
+        get { return _enabled; }
+        set
+        {
+            _enabled = value;
+        }
+    }
 }
--- LeichtNote/ViewModels/SettingsViewModels/FreifelderViewModel.cs
+++ LeichtNote/ViewModels/SettingsViewModels/FreifelderViewModel.cs
@@ -1,4 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
 using LeichtNote.Models;
+using LeichtNote.Models.SettingsModels;
+using LeichtNote.Utils;
 
 namespace LeichtNote.ViewModels.SettingsViewModels;
 
@@ -10,8 +14,36 @@ public class FreifelderViewModel
         "und geben Sie eine Bezeichnung ein.";
     
     private SettingsModel SettingsModel { get; }
+
+    private IEnumerable<DictEntry<string?, FreifelderModel>> _viewFreifelder { get; set; }
+
+    public IEnumerable<DictEntry<string?, FreifelderModel>> Freifelder
+    {
+        get { return _viewFreifelder;  }
+        set
+        {
+            _viewFreifelder = value;
+        }
+    }
+    
     public FreifelderViewModel(SettingsModel settingsModel)
     {
         SettingsModel = settingsModel;
+        _viewFreifelder = new List<DictEntry<string?, FreifelderModel>>()
+        {
+            new DictEntry<string?, FreifelderModel>("Freifelder 1", new FreifelderModel { Name = "T1", Enabled = false }),
+            new DictEntry<string?, FreifelderModel>("Freifelder 2", new FreifelderModel { Name = "T2", Enabled = false }),
+            new DictEntry<string?, FreifelderModel>("Freifelder 3", new FreifelderModel { Name = "T3", Enabled = false }),
+            new DictEntry<string?, FreifelderModel>("Freifelder 4", new FreifelderModel { Name = "T3", Enabled = false }),
+            new DictEntry<string?, FreifelderModel>("Freifelder 5", new FreifelderModel { Name = "T5", Enabled = false }),
+        };
+        // _viewFreifelder = new List<FreifelderModel>()
+        // {
+        //     new FreifelderModel { Name = "F1", Enabled = false },
+        //     new FreifelderModel { Name = "F2", Enabled = false },
+        //     new FreifelderModel { Name = "F3", Enabled = false },
+        //     new FreifelderModel { Name = "F4", Enabled = false },
+        //     new FreifelderModel { Name = "F5", Enabled = false },
+        // };
     }
 }
